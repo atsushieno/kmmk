9c528d82cb84c627463ea613fdf72abded7ac79c
atsushieno <atsushieno@gmail.com>
2021-08-02 18:58:19 +0900

    use kotlin gradle plugin 1.5.21 and AGP 4.2.0
    > e: This version (1.1.0-alpha01) of the Compose Compiler requires Kotlin version 1.5.21 but you appear to be using Kotlin version 1.5.20 which is not known to be compatible.  Please fix your configuration (or `suppressKotlinVersionCompatibilityCheck` but don't say I didn't warn you!).
    
    AGP 7.0.0 was rejected by IDEA 2021.2 as unsupported.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
-        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath("com.android.tools.build:gradle:4.2.0")
     }
 }
 
