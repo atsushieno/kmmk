8832052fec6f524292fc75f800a3b237b89e0ca1
atsushieno <atsushieno@gmail.com>
2021-08-10 01:53:02 +0900

    fix desktop window title, and modernize API usage.

--- desktop/src/jvmMain/kotlin/main.kt
+++ desktop/src/jvmMain/kotlin/main.kt
@@ -1,16 +1,20 @@
 package dev.atsushieno.kmmk
 import androidx.compose.desktop.Window
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 import java.io.File
 import dev.atsushieno.ktmidi.AlsaMidiAccess
 import dev.atsushieno.ktmidi.JvmMidiAccess
 import dev.atsushieno.ktmidi.RtMidiAccess
 import com.arkivanov.decompose.extensions.compose.jetbrains.rememberRootComponent
 
-fun main(args: Array<String>) = Window {
-    val kmmk = rememberRootComponent(factory = ::KmmkComponentContext)
-    kmmk.midiDeviceManager.midiAccess =
-        if (File("/dev/snd/seq").exists()) AlsaMidiAccess()
-        else if (args.contains("jvm")) JvmMidiAccess()
-        else RtMidiAccess()
-    App(kmmk)
+fun main(args: Array<String>) = application {
+    Window(onCloseRequest = ::exitApplication, title = "kmmk") {
+        val kmmk = rememberRootComponent(factory = ::KmmkComponentContext)
+        kmmk.midiDeviceManager.midiAccess =
+            if (File("/dev/snd/seq").exists()) AlsaMidiAccess()
+            else if (args.contains("jvm")) JvmMidiAccess()
+            else RtMidiAccess()
+        App(kmmk)
+    }
 }
