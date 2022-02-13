f7d9f9219b9cfe80e9e32e6ced38f5903f2c05be
atsushieno <atsushieno@gmail.com>
2022-02-14 01:39:39 +0900

    more graceful activity exit.

--- android/src/main/java/dev/atsushieno/kmmk/MainActivity.kt
+++ android/src/main/java/dev/atsushieno/kmmk/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import com.arkivanov.decompose.DefaultComponentContext
 import dev.atsushieno.ktmidi.AndroidMidiAccess
-import com.arkivanov.decompose.extensions.compose.jetpack.rememberRootComponent
-import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
@@ -27,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         if (System.currentTimeMillis() - lastBackPressed < 2000)
-                exitProcess(0)
+                finish()
         else
             Toast.makeText(this, "Tap once more to quit", Toast.LENGTH_SHORT).show()
         lastBackPressed = System.currentTimeMillis()
