7d29c3a3156f752a6e039e584cc0121d8946ba9a
atsushieno <atsushieno@gmail.com>
2022-10-16 14:41:00 +0900

    adjust desktop window size and title.

--- desktop/src/jvmMain/kotlin/main.kt
+++ desktop/src/jvmMain/kotlin/main.kt
@@ -2,17 +2,26 @@ package dev.atsushieno.kmmk
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.res.useResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowSize
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import androidx.compose.ui.window.singleWindowApplication
 import java.io.File
 import dev.atsushieno.ktmidi.AlsaMidiAccess
 import dev.atsushieno.ktmidi.JvmMidiAccess
 import dev.atsushieno.ktmidi.RtMidiAccess
 
-fun main(args: Array<String>) = singleWindowApplication {
-    val kmmk = KmmkComponentContext()
-    kmmk.midiDeviceManager.midiAccess =
-        if (File("/dev/snd/seq").exists()) AlsaMidiAccess()
-        else if (args.contains("jvm")) JvmMidiAccess()
-        else RtMidiAccess()
-    App(kmmk)
+fun main(args: Array<String>) = application {
+    Window(onCloseRequest = ::exitApplication,
+        title = "Kmmk: Virtual MIDI Keyboard",
+        state = rememberWindowState(width = 640.dp, height = 780.dp)) {
+        val kmmk = KmmkComponentContext()
+        kmmk.midiDeviceManager.midiAccess =
+            if (File("/dev/snd/seq").exists()) AlsaMidiAccess()
+            else if (args.contains("jvm")) JvmMidiAccess()
+            else RtMidiAccess()
+        App(kmmk)
+    }
 }
